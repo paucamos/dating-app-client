3244a64402fbe275dbaa074367dbf282c5a4693d
paucamos <paucamos98@gmail.com>
2021-01-19 08:36:26 +0100

    Added register functionality to client

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -13,12 +13,10 @@ export class AppComponent implements OnInit {
   users: any;
 
   constructor(
-    private http: HttpClient,
     private accountService: AccountService
   ) {}
 
   ngOnInit() {
-    this.getUsers();
     this.setCurrentUser();
   }
 
@@ -27,14 +25,4 @@ export class AppComponent implements OnInit {
     this.accountService.setCurrentUser(user);
   }
 
-  getUsers() {
-    this.http.get('https://localhost:5001/api/users').subscribe(
-      (response) => {
-        this.users = response;
-      },
-      (error) => {
-        console.log(error);
-      }
-    );
-  }
 }
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,3 +1,4 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -10,9 +11,14 @@ export class HomeComponent implements OnInit {
 
   constructor() {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+  }
 
   registerToggle() {
     this.registerMode = !this.registerMode;
+  }
+
+  cancelRegisterMode(event: boolean) {
+    this.registerMode = event;
   }
 }
